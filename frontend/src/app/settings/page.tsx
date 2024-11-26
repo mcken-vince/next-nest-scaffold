da0e06713ba0df75dd391ae07640f736f9316664
mcken-vince <mcken.vince@gmail.com>
2024-11-26 16:01:29 -0700

    Add Settings to TopNav

---
+++ frontend/src/app/settings/page.tsx
@@ -0,0 +1,10 @@
+import { Divider } from '@components/Dividers/Divider';
+import { SectionTitle } from '@components/SectionTitle';
+
+export default function SettingsPage() {
+  return (
+    <div className="flex flex-col mx-auto max-w-screen-xl">
+      <SectionTitle>Settings</SectionTitle>
+    </div>
+  );
+}
--- frontend/src/components/Navigation/TopNav.tsx
+++ frontend/src/components/Navigation/TopNav.tsx
@@ -10,7 +10,7 @@ export const TopNav = () => {
 
   return (
     <header className="bg-white">
-      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto flex h-16 items-center gap-8 px-4 sm:px-6 lg:px-8">
         <Link className="block text-teal-600" href="/">
           <span className="sr-only">Home</span>
           <svg
@@ -28,24 +28,31 @@ export const TopNav = () => {
 
         <div className="flex flex-1 items-center justify-end md:justify-between">
           <nav aria-label="Global" className="hidden md:block">
-            {authenticated && (
+            {/* {authenticated && (
               <ul className="flex items-center gap-6 text-sm">
                 <li>
                   <NavLink href="/" label="Home" />
                 </li>
               </ul>
-            )}
+            )} */}
           </nav>
 
           <div className="flex items-center gap-4">
             <div className="sm:flex sm:gap-4">
               {authenticated ? (
-                <button
-                  className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700 "
-                  onClick={() => signOut()}
-                >
-                  Logout
-                </button>
+                <>
+                  <ul className="flex items-center gap-6 text-sm">
+                    <li>
+                      <NavLink href="/settings" label="Settings" />
+                    </li>
+                  </ul>
+                  <button
+                    className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700 "
+                    onClick={() => signOut()}
+                  >
+                    Logout
+                  </button>
+                </>
               ) : (
                 <Link
                   className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
