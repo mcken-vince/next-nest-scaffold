6b6f3cc821319cc9efeef6132800a90b2537072e
mcken-vince <mcken.vince@gmail.com>
2024-11-27 20:03:49 -0700

    Add UserSettingsModule to AppModule imports

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from '../core/database/database.module';
 import { AuthModule } from '../modules/auth/auth.module';
+import { UsersModule } from '../modules/users/users.module';
+import { UserSettingsModule } from '../modules/user-settings/user-settings.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     AuthModule,
+    UsersModule,
+    UserSettingsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
