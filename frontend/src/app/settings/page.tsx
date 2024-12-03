5f5279dcd26e382985b31236f7ce52b161bdbe9e
mcken-vince <mcken.vince@gmail.com>
2024-12-02 21:05:34 -0700

    Fetch user settings by id - temporary until header context is setup in api handler

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,10 +1,23 @@
+'use client';
+
 import { Divider } from '@components/Dividers/Divider';
 import { SectionTitle } from '@components/SectionTitle';
+import { apiFetch } from '@helpers/clients';
+import { useQuery } from '@tanstack/react-query';
+import { useSession } from 'next-auth/react';
 
 export default function SettingsPage() {
+  const { data: session } = useSession();
+  const { data, error } = useQuery({
+    queryKey: ['settings'],
+    queryFn: async () => {
+      return apiFetch(`/user-settings/${session?.user?.id}`);
+    },
+  });
   return (
     <div className="flex flex-col mx-auto max-w-screen-xl">
       <SectionTitle>Settings</SectionTitle>
+      {JSON.stringify(data)}
     </div>
   );
 }
